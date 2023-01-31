bea5a85fff1eb71d19b3ad2e7a5d273ce45b1a0e
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-01-31 16:38:41 -0500

    lecture-2

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,17 +1,34 @@
-//BookStore has been moved to data.js 
+//BookStore has been moved to data.js
 console.log(bookStore);
 
+const bookList = document.querySelector("#book-list");
+
 function formatPrice(price) {
-  return '$' + Number.parseFloat(price).toFixed(2);
+  return "$" + Number.parseFloat(price).toFixed(2);
 }
 
-// create a function called renderBook(book)
-// it will take a book object as an argument
-// and create the html struture for rendering 
-// that book and insert it into our webpage!
+function renderHeader(bookStore) {
+  document.querySelector("#store-name").textContent = bookStore.name;
+}
 
-// function renderBook(book) {
-// should create an li element that looks something like this:
+renderHeader(bookStore);
+
+function renderFooter(bookStore) {
+  document.querySelector("#location").textContent = bookStore.location;
+  document.querySelector("#number").textContent = bookStore.number;
+  document.querySelector("#address").textContent = bookStore.address;
+  document.querySelector("#hours").textContent = bookStore.hours;
+}
+
+renderFooter(bookStore);
+
+function renderBook(book) {
+  // create a function called renderBook(book)
+  // it will take a book object as an argument
+  // and create the html struture for rendering
+  // that book and insert it into our webpage!
+  // function renderBook(book) {
+  // should create an li element that looks something like this:
   // <li class="list-li">
   //   <h3>Eloquent JavaScript : A Modern Introduction to Programming</h3>
   //   <p>Marjin Haverbeke</p>
@@ -20,4 +37,31 @@ function formatPrice(price) {
   //   <button>Delete</button>
   // </li>
 
-  
+  const li = document.createElement("li");
+  li.className = "list-li";
+
+  const h3 = document.createElement("h3");
+  h3.textContent = book.title;
+  li.append(h3);
+
+  const pAuthor = document.createElement("p");
+  pAuthor.textContent = book.author;
+  li.append(pAuthor);
+
+  const price = document.createElement("p");
+  price.textContent = formatPrice(book.price);
+  li.append(price);
+
+  const img = document.createElement("img");
+  img.src = book.imageUrl;
+  img.alt = `${book.title} cover`;
+  li.append(img);
+
+  const button = document.createElement("button");
+  button.textContent = "Delete";
+  li.append(button);
+
+  bookList.append(li);
+}
+
+bookStore.inventory.forEach((book) => renderBook(book));
